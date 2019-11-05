dee7838c0b1483ee6cb048ed27ad8f6ed8889266
Itap1999 <mi96!u72?>
2019-11-05 12:28:08 +0100

    asdfsdr

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basis_app/screens/HavaneserScreen.dart';
+import 'package:flutter_basis_app/screens/beagle_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,8 +10,56 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+            leading: Icon(Icons.menu),
+            title: Text("Home"),
+            actions: <Widget>[
+              Icon(Icons.party_mode),
+            ]
+        ),
+
+        body: Center(
+          child: Column(
+    children: <Widget>[
+    RaisedButton(
+          onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HavaneserScreen(),
+              ),
+            );
+          },
+          child: Text("Havaneser"),
+
+    ),
+    buildRaisedHunderasseButton(context, "Beagle", BeagleScreen()),
+    buildRaisedHunderasseButton(context, "moinsen", BeagleScreen())
+
+      ,
+    ]
+    ,
+    ),
+        )
+    ,
+    );
 
 
+  }
+
+  Widget buildRaisedHunderasseButton(BuildContext context, text, widget) {
+    return RaisedButton(
+    onPressed: (){
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => widget,
+        ),
+      );
+    },
+    child: Text(text),
+
+  );
   }
 }
